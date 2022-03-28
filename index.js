4515e2ac0ea2dec37375acb64db57d2bddcd57c4
Paschal Ramdan <dane.ramdan@gmail.com>
2022-03-28 16:55:55 -0400

    commit tree fb33275da9d7c722979ffe810f3389a8506c2d28

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left + 1}px`;
+    }
+  });
+
+
+// //   function moveDodgerRight() {
+// //     var leftNumbers = dodger.style.left.replace('px', '');
+// //     var left = parseInt(leftNumbers, 10)
+// //     if (left < 360) {
+// //       dodger.style.left = `${left + 1}px`
+// //     }
+//   }
